5520f9a8f323809a925adbab632f27dec918723f
Julia Tkachenko <julia.tkachenko@ewn.com>
2026-04-09 14:37:51 -0500

    add 2 and 3 API

--- styles/script.js
+++ styles/script.js
@@ -1,2 +1,4 @@
 console.log("Hello, World!");
- const API 1 = "https://api.example.com/data";``
+const API 1 = "https://api.example.com/data";
+const API 2 = "https://api.example.com/data 2 ";
+const API 3 = "https://api.example.com/data 3 ";
